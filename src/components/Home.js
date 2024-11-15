e806976e39ca07ff262fb52e72d00019cec898d4
Marion1b <marion.barthoux@gmail.com>
2024-11-15 16:25:12 +0100

    add animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,34 @@ function Home() {
     return (
         <section className="home">
             <img src={image} alt="" />
-            <p className='big'>Bonjour,  je suis <strong>Marion Barthoux</strong> <br /> 
-            développeuse web fullstack</p>
+            <p className='big'>Bonjour,  je suis Marion Barthoux <br /> 
+            <strong>
+                <span className="home-key">d</span>
+                <span className="home-key">é</span>
+                <span className="home-key">v</span>
+                <span className="home-key">e</span>
+                <span className="home-key">l</span>
+                <span className="home-key">o</span>
+                <span className="home-key">p</span>
+                <span className="home-key">p</span>
+                <span className="home-key">e</span>
+                <span className="home-key">u</span>
+                <span className="home-key">s</span>
+                <span className="home-key">e</span>&nbsp;
+                <span className="home-key">w</span>
+                <span className="home-key">e</span>
+                <span className="home-key">b</span>&nbsp;
+                <span className="home-key">f</span>
+                <span className="home-key">u</span>
+                <span className="home-key">l</span>
+                <span className="home-key">l</span>
+                <span className="home-key">s</span>
+                <span className="home-key">t</span>
+                <span className="home-key">a</span>
+                <span className="home-key">c</span>
+                <span className="home-key">k</span>
+
+            </strong></p>
         </section>
     );
 }
