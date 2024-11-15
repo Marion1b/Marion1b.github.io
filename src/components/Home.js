bd0f5338e33872455f3c3febe8ae7711ed5ba317
Marion1b <marion.barthoux@gmail.com>
2024-11-15 14:51:31 +0100

    mobile responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
-import image from '../assets/images/image.jpg';
+import image from '../assets/images/home-illu.svg';
 
 function Home() {
     return (
         <section className="home">
             <img src={image} alt="" />
-            <p className='big'>Bonjour,  je suis <strong>Marion Barthoux</strong> développeuse web fullstack</p>
+            <p className='big'>Bonjour,  je suis <strong>Marion Barthoux</strong> <br /> 
+            développeuse web fullstack</p>
         </section>
     );
 }
